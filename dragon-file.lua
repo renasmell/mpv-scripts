eb97e7d3790869db5564865b0dfb020dae0496e7
renascented <140450870+renasmell@users.noreply.github.com>
2026-03-18 18:47:44 -0400

    Update dragon-file.lua

--- dragon-file.lua
+++ dragon-file.lua
@@ -6,6 +6,8 @@ require "mp"
 local MAX_SIZE = 10000000 -- discord 10MB limit, feel free to remove or change
 local force_upload = false
 
+-- in input.conf
+-- ctrl+d script-binding dragon-video
 mp.add_key_binding(nil, "dragon-video", function ()
    if mp.get_property_native("file-size") > MAX_SIZE and not force_upload then
       mp.osd_message("large, retry to force")
