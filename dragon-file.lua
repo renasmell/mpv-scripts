46bed445d2daab2f50f1147fddd03c2d43948414
renascented <140450870+renasmell@users.noreply.github.com>
2026-03-18 18:46:52 -0400

    Create dragon-file.lua

---
+++ dragon-file.lua
@@ -0,0 +1,18 @@
+-- calls dragon command on current file
+-- assumes you have https://github.com/krayon/dragon-drag-drop-tool
+
+require "mp"
+
+local MAX_SIZE = 10000000 -- discord 10MB limit, feel free to remove or change
+local force_upload = false
+
+mp.add_key_binding(nil, "dragon-video", function ()
+   if mp.get_property_native("file-size") > MAX_SIZE and not force_upload then
+      mp.osd_message("large, retry to force")
+      force_upload = true
+   else
+      mp.command("show-text \"size: " .. "${file-size}\"")
+      -- might need to put path to dragon executable
+      os.execute("dragon --and-exit \""..mp.get_property_native("path").."\"")
+   end
+end)
